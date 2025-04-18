33913f0bf05f9e628af2690dcbff5dbf6c5f1f9d
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-04-18 11:51:15 -0400

    Removed junk code

--- model.py
+++ model.py
@@ -6,19 +6,11 @@ import numpy as np
 
 PATH = os.path.join('data', 'credit.csv')
 df = pandas.read_csv(PATH)
-temp = df.copy()
 
 df['Credit History'] = df['Credit History'].astype('category').cat.codes
 df['Debt'] = df['Debt'].astype('category').cat.codes
 df['Collateral'] = df['Collateral'].astype('category').cat.codes
 df['Credit Risk?'] = df['Credit Risk?'].astype('category').cat.codes
-print(df)
-
-temp['Credit History'] = temp['Credit History'].map({'bad': 0, 'good': 1, 'unknown': 2})
-temp['Debt'] = temp['Debt'].map({'high': 0, 'low': 1})
-temp['Collateral'] = temp['Collateral'].map({'adequate': 0, 'none': 1})
-temp['Credit Risk?'] = temp['Credit Risk?'].map({'high': 0, 'low': 1})
-print(temp)
 
 features = ['Credit History', 'Debt', 'Collateral']
 X = df[features]
