a1cd573000a2ae2283f644d55451d06a2f91c243
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-04-18 11:50:44 -0400

    Changed converting method

--- model.py
+++ model.py
@@ -6,12 +6,19 @@ import numpy as np
 
 PATH = os.path.join('data', 'credit.csv')
 df = pandas.read_csv(PATH)
+temp = df.copy()
 
-# Convert categorical data to discrete numerical
-df['Credit History'] = df['Credit History'].map({'good': 0, 'bad': 1, 'unknown': 2})
-df['Debt'] = df['Debt'].map({'low': 0, 'high': 1})
-df['Collateral'] = df['Collateral'].map({'adequate': 0, 'none': 1})
-df['Credit Risk?'] = df['Credit Risk?'].map({'low': 0, 'high': 1})
+df['Credit History'] = df['Credit History'].astype('category').cat.codes
+df['Debt'] = df['Debt'].astype('category').cat.codes
+df['Collateral'] = df['Collateral'].astype('category').cat.codes
+df['Credit Risk?'] = df['Credit Risk?'].astype('category').cat.codes
+print(df)
+
+temp['Credit History'] = temp['Credit History'].map({'bad': 0, 'good': 1, 'unknown': 2})
+temp['Debt'] = temp['Debt'].map({'high': 0, 'low': 1})
+temp['Collateral'] = temp['Collateral'].map({'adequate': 0, 'none': 1})
+temp['Credit Risk?'] = temp['Credit Risk?'].map({'high': 0, 'low': 1})
+print(temp)
 
 features = ['Credit History', 'Debt', 'Collateral']
 X = df[features]
@@ -20,8 +27,8 @@ Y = df['Credit Risk?']
 dtree = tree.DecisionTreeClassifier(criterion="log_loss")
 dtree = dtree.fit(X, Y)
 # print(dtree.predict(np.array(df.iloc[12])[1:4].reshape(1, -1)))
-print(dtree.predict(np.array([0, 1, 0]).reshape(1, -1)))
-print(dtree.predict(np.array([2, 0, 0]).reshape(1, -1)))
+print(dtree.predict(np.array([1, 0, 0]).reshape(1, -1)))
+print(dtree.predict(np.array([2, 1, 0]).reshape(1, -1)))
 
 tree.plot_tree(dtree, feature_names=features)
 plt.show()
